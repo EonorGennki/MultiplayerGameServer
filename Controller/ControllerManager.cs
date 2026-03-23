d92a4637ac52da074324966af9c0d637a14a88d8
Eonor <2195296822@qq.com>
2026-03-23 18:39:22 +0800

    添加注释

--- Controller/ControllerManager.cs
+++ Controller/ControllerManager.cs
@@ -20,6 +20,11 @@ namespace MultiplayerGameServer.Controller
             controlDic.Add(userController.GetRequestCode, userController);
         }
 
+        /// <summary>
+        /// 根据请求码和行为码，调用对应的controller方法
+        /// </summary>
+        /// <param name="pack">消息包</param>
+        /// <param name="client">发送消息的客户端对象</param>
         public void HandleRequest(MainPack pack, Client client)
         {
             if (controlDic.TryGetValue(pack.RequestCode, out BaseController controller))
@@ -28,7 +33,7 @@ namespace MultiplayerGameServer.Controller
                 MethodInfo? method = controller.GetType().GetMethod(methodName);
                 if (method is null)
                 {
-                    Console.WriteLine("未找到指定方法" + pack.ActionCode.ToString());
+                    Console.WriteLine("未找到指定方法：" + pack.ActionCode.ToString());
                     return;
                 }
 
@@ -41,7 +46,7 @@ namespace MultiplayerGameServer.Controller
             }
             else
             {
-                Console.WriteLine("未找到指定controller"+ pack.RequestCode.ToString());
+                Console.WriteLine("未找到指定controller："+ pack.RequestCode.ToString());
             }
         }
     }
